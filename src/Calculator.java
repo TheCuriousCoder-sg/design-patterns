2264f069f6fae6d8610d3458c5f8ed5672bf91e8
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2026-05-31 00:28:01 +0530

    Singleton Pattern

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,24 @@ public class Calculator {
     int a;
     int b;
 
+    private static Calculator obj;
+
+    private Calculator() {
+        System.out.println("Instance Created");
+    }
+
     public int sum() {
         return a+b;
+    }
+
+    public static Calculator getInstance() {
+        if(obj == null) {
+            synchronized (Calculator.class) {
+                if(obj == null) {
+                    obj = new Calculator();
+                }
+            }
+        }
+        return obj;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+//        Calculator c1 = Calculator.getInstance();
+//        Calculator c2 = Calculator.getInstance();
+//
+//        c1.a = 10;
+//        c1.b= 5;
+//
+//        c2.a=7;
+//        c2.b=3;
+//
+//        System.out.println(c1.sum());
+//        System.out.println(c2.sum());
 
+        Thread t1 = new Thread(() -> {
+            Calculator.getInstance();
+        });
+
+        Thread t2 = new Thread(() -> {
+            Calculator.getInstance();
+        });
+
+        t1.start();
+        t2.start();
     }
 }
