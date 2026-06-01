d39d2ed8fa893f0e4c3f3ad526ece26840f6e48d
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2026-06-01 13:38:44 +0530

    Prototype Pattern

--- src/GameBotCharacters.java
+++ src/GameBotCharacters.java
@@ -1,9 +1,13 @@
-public class GameBotCharacters {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameBotCharacters implements Cloneable<GameBotCharacters>{
     private String name;
     private int health;
     private int attackPower;
+    private List<String> weapons;
 
-    public GameBotCharacters(String name, int health, int attackPower) {
+    public GameBotCharacters(String name, int health, int attackPower, List<String> weapons) {
 
         // Expensive Operations
         System.out.println("Loading character animations from DB...");
@@ -19,6 +23,19 @@ public class GameBotCharacters {
         this.name = name;
         this.health = health;
         this.attackPower = attackPower;
+        this.weapons = weapons;
+    }
+
+    private GameBotCharacters(GameBotCharacters gbc) {
+        this.name = gbc.name;
+        this.health = gbc.health;
+        this.attackPower = gbc.attackPower;
+        this.weapons = new ArrayList<>(gbc.weapons);
+    }
+
+    @Override
+    public GameBotCharacters customizedClone() {
+        return new GameBotCharacters(this);
     }
 
     @Override
@@ -27,6 +44,35 @@ public class GameBotCharacters {
                 "name='" + name + '\'' +
                 ", health=" + health +
                 ", attackPower=" + attackPower +
+                ", weapons=" + weapons +
                 '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public List<String> getWeapons() {
+        return weapons;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
         long startTime = System.currentTimeMillis();
 
         GameBotCharacters gbc1 = new GameBotCharacters("Bot1", 100,
-                30);
-        GameBotCharacters gbc2 = new GameBotCharacters("Bot2", 100,
-                30);
-        GameBotCharacters gbc3 = new GameBotCharacters("Bot3", 100,
-                30);
-        GameBotCharacters gbc4 = new GameBotCharacters("Bot4", 100,
-                30);
-        GameBotCharacters gbc5 = new GameBotCharacters("Bot5", 100,
-                20);
+                0, new ArrayList<>(Arrays.asList("Rifle")));
+        GameBotCharacters gbc2 = gbc1.customizedClone();
+        gbc2.setName("Bot2");
+        GameBotCharacters gbc3 = gbc1.customizedClone();
+        gbc3.setName("Bot3");
+        GameBotCharacters gbc4 = gbc1.customizedClone();
+        gbc4.setName("Bot4");
+        GameBotCharacters gbc5 = gbc1.customizedClone();
+        gbc5.setName("Bot5");
+        List<String> weapons = gbc5.getWeapons();
+        weapons.add("Sniper");
+
+        System.out.println(gbc1);
+        System.out.println(gbc2);
+        System.out.println(gbc3);
+        System.out.println(gbc4);
+        System.out.println(gbc5);
 
         long endTime = System.currentTimeMillis();
 
-        System.out.println(endTime-startTime);
+        System.out.println("Total Time Taken" + (endTime-startTime));
     }
 }
