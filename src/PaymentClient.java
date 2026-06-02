9e7e53ef2b48d97def87ecb46eeea9bd002e245e
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2026-06-02 17:01:29 +0530

    Initial Commit

--- src/PaymentClient.java
+++ src/PaymentClient.java
@@ -2,7 +2,7 @@ public class PaymentClient {
     PaymentService payment = new PaymentService();
 
     public void makePayment() {
-        String result = payment.pay(100.0, 123);
+        String result = payment.pay(100, 123);
 
         if(result.equals("SUCCESS")) {
             System.out.println("Payment done successfully");
